9aa46c9fa1476d7eb8bb2766b65bc45f7bb9762b
Manuel Alejandro <lonperman@gmail.com>
2020-09-09 01:34:16 -0500

    Ajustes a la ingenua

--- src/selecciones/Ingenua.java
+++ src/selecciones/Ingenua.java
@@ -13,12 +13,17 @@ public class Ingenua {
     
     public void ingenua() {
     
-        int Matriz_selecciones[][] = {{0,1,2,3,4,5},
-                                      {1,0,1,0,1,1},
-                                      {2,1,0,1,0,1},
-                                      {3,0,1,0,1,1},
-                                      {4,0,1,1,0,1},
-                                      {5,1,1,1,1,0}};
+        int Matriz_selecciones[][] = {{0,1,2,3,4,5,6,7,8,9,10},
+                                      {1,0,0,1,0,0,0,1,0,0,1},
+                                      {2,0,0,1,0,0,0,0,1,0,0},
+                                      {3,1,1,0,0,0,0,0,0,1,1},
+                                      {4,0,0,0,0,0,1,1,1,0,1},
+                                      {5,0,0,0,0,0,0,0,0,0,1},
+                                      {6,0,0,0,1,0,0,0,0,0,1},
+                                      {7,1,0,0,1,0,0,0,0,0,0},
+                                      {8,0,1,0,1,0,0,0,0,0,1},
+                                      {9,0,0,1,0,0,0,0,0,0,1},
+                                      {10,1,0,1,1,1,1,0,1,1,0}};
         
         int TamañoX = Matriz_selecciones[0].length-1;
         int TamañoY = Matriz_selecciones.length;
@@ -47,6 +52,7 @@ public class Ingenua {
         int Contador = 0 ;
         int Contador2 = Selecciones_Salida.length - 1;
         int Cantidad = 0;
+        boolean Enemigo = false;
         //Comparador(1, Matriz_NR, 2);
         while(!Solucion){
          
@@ -56,7 +62,7 @@ public class Ingenua {
             }else if(Posicion_2 <= Selecciones_Salida.length) {
                 Elemento = Selecciones_Salida[Posicion_2][0];
              //   Contador = 0;
-               System.out.println("Elemento Seleccionado: " + Elemento);
+             //  System.out.println("Elemento Seleccionado: " + Elemento);
                 
             }
            //For que me verifica si la seleccion a sido asignada en una villa
@@ -67,7 +73,7 @@ public class Ingenua {
                     Contador++;
                 }
             }
-            System.out.println("Tamaño Contador: " + Contador);
+            //System.out.println("Tamaño Contador: " + Contador);
             //Si la villa tiene la cantidad de 0 que la matriz quiere decir que no a sido asignada
             if(Contador == TamañoX-1){
             //Recorre toda la matriz de salida    
@@ -75,20 +81,22 @@ public class Ingenua {
              //Verifica si en el la matriz hay selecciones asignadas
                 if(Selecciones_Salida[i][Posicion_1] == 1){
                   //Se envia los datos para comparar los enemigos
-                    System.out.println("Seleccion: " + Selecciones_Salida[i][0]+ "Elemento: " + Elemento);
+                  //  System.out.println("Seleccion: " + Selecciones_Salida[i][0]+ " Elemento: " + Elemento);
                   //Se pregunta si el elemento es diferente de su seleccion  
                    if(Selecciones_Salida[i][0] != Elemento){
                        
                       Respuesta = Comparador(Selecciones_Salida[i][0], Matriz_NR, Elemento);
                  //Si el elemento no esta dentro de los enemigos de la seleccion la respuesta sera un 1
-                   if(Respuesta == 1){
+                       //System.out.println("Respuesta: " + Respuesta);
+                   if(Respuesta == 1 && Enemigo == false){
                        //Se asigna el elemento en la matriz de salida
                        Selecciones_Salida[Posicion_2][Posicion_1] = 1;
                        //El contador lleva la cantidad de elemntos que no se han seleccionado
-                       Contador2 = Contador2 - 1;
+                       
                  //Si el elemnto esta dentro de los enemigos de la seleccion la respuesta sera 2
                    } else if(Respuesta == 2) {
                       Selecciones_Salida[Posicion_2][Posicion_1] = 0;
+                      Enemigo = true;
                    }
                   
                    }
@@ -101,26 +109,33 @@ public class Ingenua {
                         Selecciones_Salida[Posicion_2][Posicion_1]= 1;
                         Contador2 = Contador2 - 1;
                         Contador = 0;
+                        Cantidad = 0;
                   
                 }
             }
             } else {
                 Contador=0;
+                Contador2 = Contador2-1;
             }
             
-          //  System.out.println("Contador Final: " + Contador2);
+           
+       //   System.out.println("Contador Final: " + Contador2);
           //Se aumenta la posicion vertical para poder seleccionar las diferentes selecciones  
             Posicion_2 = Posicion_2 + 1;
           //  System.out.println("Posicion 2: " + Posicion_2);
           //Se coloca la cantidad en 0 para poder realizar comparaciones despues dentro el for  
             Cantidad = 0;
+           
+            Enemigo = false;
             
          
       //Verifica si hay mas elementos que no han sido asignados
             if(Contador2 > 0 && Elemento == TamañoY-1){
-                System.out.println("Entrando al else");
+            //    System.out.println("Entrando al else");
                 Posicion_1++;
+                Posicion_2 = 1;
                 Elemento = 0;
+                Contador2 = Selecciones_Salida.length - 1;
                 System.out.println("Posicion 1: " + Posicion_1);
       //Si la posicion sobre pasa el tamaño de la matriz se finalizara el bucle         
             }if(Posicion_1 >= TamañoX){
@@ -138,11 +153,12 @@ public class Ingenua {
         
         for(int i = 0;i < Selecciones_Salida.length;i++){
             //System.out.println("Seleccion: " + Selecciones_Salida[i][Posicion_1]);
-            for(int k = 0; k < Selecciones_Salida[0].length;k++){
+            for(int k = 0; k <= Posicion_1;k++){
                  System.out.print(Selecciones_Salida[i][k]+" ");            
             }
            System.out.println();
         }
+        
         System.out.println("--------------------------------------------------------------------");
         System.out.println();
          for(int i = 0;i < Matriz_NR.length;i++){
